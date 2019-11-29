f3b696a8bafbac25cb862a84163b96d98bc92eca
myflavor <45022747+myflavor@users.noreply.github.com>
2019-11-29 15:38:31 +0800

    add unblockmusic

--- diy.sh
+++ diy.sh
@@ -5,5 +5,5 @@
 #   Author: P3TERX
 #   Blog: https://p3terx.com
 #=================================================
-# Modify default IP
+git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
 sed -i '14s/bootstrap/argon/' feeds/luci/collections/luci/Makefile
