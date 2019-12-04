b9cf32590cd3ffbe3a42270555e6ae9648bb5df6
myflavor <45022747+myflavor@users.noreply.github.com>
2019-12-04 16:56:29 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,5 +5,5 @@
 #   Author: P3TERX
 #   Blog: https://p3terx.com
 #=================================================
-git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
+git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic package/luci-app-unblockneteasemusic
 sed -i '14s/bootstrap/argon/' feeds/luci/collections/luci/Makefile
