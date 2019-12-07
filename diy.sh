e33ba626a8570d2a631e34f1b4133cd38f8399c8
MyFlavor <45022747+myflavor@users.noreply.github.com>
2019-12-07 13:14:50 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,8 +10,10 @@
 git reset --hard  db5d264ccfd7b8b2a1bf48b740027bd5d448a83f
 #add another unblockneteasemusic with some changes
 git clone https://github.com/myflavor/luci-app-unblockneteasemusic package/luci-app-unblockneteasemusic
+chmod -R +x package/luci-app-unblockneteasemusic
 #add adbyby plus with some changes
 rm -rf package/lean/luci-app-adbyby-plus
 git clone https://github.com/myflavor/luci-app-adbyby-plus package/lean/luci-app-adbyby-plus
+chmod -R +x package/lean/luci-app-adbyby-plus
 #change the default theme to material
 sed -i '14s/bootstrap/material/' feeds/luci/collections/luci/Makefile
