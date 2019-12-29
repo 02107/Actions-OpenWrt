62bda5fcfc300397254ea4263404101fdad1c3a2
MyFlavor <45022747+myflavor@users.noreply.github.com>
2019-12-29 19:34:22 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,15 +6,15 @@
 #   Blog: https://p3terx.com
 #=================================================
 #add another unblockneteasemusic with some changes
-git clone https://github.com/myflavor/luci-app-unblockneteasemusic package/luci-app-unblockneteasemusic
-chmod -R +x package/luci-app-unblockneteasemusic
+#git clone https://github.com/myflavor/luci-app-unblockneteasemusic package/luci-app-unblockneteasemusic
+#chmod -R +x package/luci-app-unblockneteasemusic
 #add adbyby plus with some changes
-rm -rf package/lean/luci-app-adbyby-plus
-git clone https://github.com/myflavor/luci-app-adbyby-plus package/lean/luci-app-adbyby-plus
-chmod -R +x package/lean/luci-app-adbyby-plus
+#rm -rf package/lean/luci-app-adbyby-plus
+#git clone https://github.com/myflavor/luci-app-adbyby-plus package/lean/luci-app-adbyby-plus
+#chmod -R +x package/lean/luci-app-adbyby-plus
 #back to old ssr plus version
-rm -rf package/lean/luci-app-ssr-plus
-git clone https://github.com/myflavor/luci-app-ssr-plus package/lean/luci-app-ssr-plus
+#rm -rf package/lean/luci-app-ssr-plus
+#git clone https://github.com/myflavor/luci-app-ssr-plus package/lean/luci-app-ssr-plus
 chmod -R +x package/lean/luci-app-ssr-plus
 #change the default theme to material
 sed -i '14s/bootstrap/material/' feeds/luci/collections/luci/Makefile
