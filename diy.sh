279fb6ea5d473fd60322d3f6f72fe49abf1c2b0d
MyFlavor <45022747+myflavor@users.noreply.github.com>
2020-01-20 16:53:32 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -3,4 +3,3 @@ git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic packag
 sed -i '14s/bootstrap/material/' feeds/luci/collections/luci/Makefile
 sed -i 's/OpenWrt/G-DOCK/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/G-DOCK/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
