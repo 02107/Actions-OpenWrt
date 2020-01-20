27801eeef201b06f1ba69f4f24f9072d2c9da17b
MyFlavor <45022747+myflavor@users.noreply.github.com>
2020-01-20 16:49:16 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
-#=================================================
-#   Description: DIY script
-#   Lisence: MIT
-#   Author: P3TERX
-#   Blog: https://p3terx.com
-#=================================================
-#add another unblockneteasemusic with some changes
 git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic package/luci-app-unblockneteasemusic
-#chmod -R +x package/luci-app-unblockneteasemusic
-#add adbyby plus with some changes
-#rm -rf package/lean/luci-app-adbyby-plus
-#git clone https://github.com/myflavor/luci-app-adbyby-plus package/lean/luci-app-adbyby-plus
-#chmod -R +x package/lean/luci-app-adbyby-plus
-#back to old ssr plus version
-#rm -rf package/lean/luci-app-ssr-plus
-#git clone https://github.com/myflavor/luci-app-ssr-plus package/lean/luci-app-ssr-plus
-#chmod -R +x package/lean/luci-app-ssr-plus
-#change the default theme to material
 sed -i '14s/bootstrap/material/' feeds/luci/collections/luci/Makefile
 sed -i 's/OpenWrt/G-DOCK/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/G-DOCK/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
