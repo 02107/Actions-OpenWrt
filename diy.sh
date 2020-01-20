7f9d8636efc44d30d9714990b5c1b80067e1936e
MyFlavor <45022747+myflavor@users.noreply.github.com>
2020-01-20 16:43:49 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -18,3 +18,6 @@ git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic packag
 #chmod -R +x package/lean/luci-app-ssr-plus
 #change the default theme to material
 sed -i '14s/bootstrap/material/' feeds/luci/collections/luci/Makefile
+sed -i 's/OpenWrt/G-DOCK/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/G-DOCK/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
